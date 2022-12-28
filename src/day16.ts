b3752e1ef225f95995c71e7b1e66e80e84f1f6bc
Cyprien Pannier <cyprien.pannier@gorgias.com>
2022-12-28 16:42:56 +0100

    day16 part 2

--- src/day16.ts
+++ src/day16.ts
@@ -1,5 +1,4 @@
-import * as _ from 'lodash'
-import { PriorityQueue } from 'typescript-collections'
+import _ = require("lodash")
 
 export const example = `Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
 Valve BB has flow rate=13; tunnels lead to valves CC, AA
@@ -12,13 +11,11 @@ Valve HH has flow rate=22; tunnel leads to valve GG
 Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II`
 
-const log = false
-
 type Graph = {
     [from: string]: {
         rate: number,
-        links: string[],
-        pressureSoFar?: number
+        links: string[]
+        dists: { [to: string]: number },
     }
 }
 
@@ -29,16 +26,7 @@ type State = {
     pressureToRelease: number
     pressure: number
     time: number
-    openedValves: Set<string>
-    lastAction: Action
-    path?: Action[]
-}
-
-function areAllValvesOpened(g: Graph, state: State): boolean {
-    for (const k in g) {
-        if (g[k].rate > 0 && !state.openedValves.has(k)) return false
-    }
-    return true
+    openedValves: string[]
 }
 
 function parseInput(input: string): Graph {
@@ -54,104 +42,156 @@ function parseInput(input: string): Graph {
     }), {})
 }
 
-function nextActions(g: Graph, state: State): Action[] {
+// Floyd–Warshall algorithm
+function compressGraph(g: Graph) {
+    for (const valve in g) {
+        g[valve].dists = {}
+        if (valve !== 'AA' && g[valve].rate === 0) continue
+        const visited = new Set()
+        const q: [number, string][] = [[0, valve]]
+        while (q.length > 0) {
+            const [dist, v] = q.shift()!
+            if (visited.has(v)) continue
+            visited.add(v)
+            if (g[v].rate > 0) {
+                g[valve].dists[v] = dist
+            }
+            g[v].links.forEach(neighbor => q.push([dist + 1, neighbor]))
+        }
+        delete g[valve].dists[valve]
+    }
+    for (const valve in g) {
+        if (valve !== 'AA' && g[valve].rate === 0) {
+            delete g[valve]
+        }
+    }
+}
+
+function nextActions(g: Graph, state: State, maxTurns: number, openableValves: Set<string>): Action[] {
     const actions: Action[] = []
-    if (state.time >= 30 || areAllValvesOpened(g, state)) return []
-    if (!state.openedValves.has(state.currentValve) && g[state.currentValve].rate > 0) {
+    if (state.time >= maxTurns) return []
+    if (openableValves.has(state.currentValve) && !state.openedValves.includes(state.currentValve) && g[state.currentValve].rate > 0) {
         actions.push(['open', state.currentValve])
     }
-    g[state.currentValve].links.forEach(to => {
+    for (const to in g[state.currentValve].dists) {
+        const distance = g[state.currentValve].dists[to]
+        if ((state.time + distance) >= maxTurns - 1) continue
         actions.push(['move', to])
-    })
+    }
     return actions
 }
 
 function applyAction(g: Graph, state: State, action: Action): State {
     const [actionType, valve] = action
+    const elapsedTime = actionType === 'move' ? g[state.currentValve].dists[valve] : 1
     return {
         currentValve: actionType === 'move' ? valve : state.currentValve,
-        pressure: state.pressure + state.pressureToRelease,
+        pressure: state.pressure + state.pressureToRelease * elapsedTime,
         pressureToRelease: actionType === 'open' ? state.pressureToRelease + g[valve].rate : state.pressureToRelease,
-        openedValves: actionType === 'open' ? new Set(state.openedValves).add(valve) : state.openedValves,
-        time: state.time + 1,
-        lastAction: action,
-        path: log ? [...(state.path || []), action] : undefined
+        openedValves: actionType === 'open' ? [...state.openedValves, valve] : state.openedValves,
+        time: state.time + elapsedTime,
     }
 }
 
-// At each move to valve step, given the currently opened valves, 
-// if the same valve has been already explored sooner (with inferior state.time) 
-// and with a bigger pressure projecting to time = now
-// => skip it
-// For this to work the graph should be explored chronologically (strictly increasing state.time)
 class PruningStrategy {
-    private explored: { [hashOpenedCurrentValve: string]: State } = {}
+    public explored = {} as { [k: string]: number }
     explore(state: State) {
-        if (state.lastAction[0] !== 'move') return
-        const hash: string = Array.from(state.openedValves).sort().join(',') + ',' + state.currentValve
-        const hashState = this.explored[hash]
-        if (!hashState) {
-            this.explored[hash] = state
-        } else {
-            const projectedPressure = hashState.pressure + (state.time - hashState.time) * hashState.pressureToRelease
-            if (projectedPressure > hashState.pressure) {
-                this.explored[hash] = state
-            }
-        }
+        const hash: string = state.openedValves.join(',') + ',' + state.currentValve
+        this.explored[hash] = state.time
     }
     shouldPrune(state: State) {
-        if (state.lastAction[0] !== 'move') return false
-        const hash: string = Array.from(state.openedValves).sort().join(',') + ',' + state.currentValve
-        const hashState = this.explored[hash]
-        if (hashState == null) return false
-        const projectedPressure = hashState.pressure + (state.time - hashState.time) * hashState.pressureToRelease
-        return projectedPressure >= state.pressure
+        const hash: string = state.openedValves.join(',') + ',' + state.currentValve
+        return state.time >= (this.explored[hash] ?? Number.MAX_SAFE_INTEGER)
     }
 }
 
-function run(g: Graph): number {
-    const q = new PriorityQueue<State>((a, b) => b.time - a.time)
-    q.add({
+function run(g: Graph, maxTurns: number, openableValves: Set<string>): number {
+    const q: State[] = [{
         currentValve: 'AA',
         pressure: 0,
         pressureToRelease: 0,
         time: 1,
-        openedValves: new Set<string>(),
-        lastAction: ['move', 'AA'],
-    })
+        openedValves: [],
+    }]
     const pruningStrategy = new PruningStrategy()
     let maxPressure = 0
-    let maxState = q.peek()
+    let maxState
     let explored = 0
-    while (!q.isEmpty()) {
-        const state = q.dequeue()!
+    while (q.length > 0) {
+        const state = q.pop()!
         if (pruningStrategy.shouldPrune(state)) continue
         pruningStrategy.explore(state)
         explored += 1
-        const nextPressure = state.pressure + (30 - state.time + 1) * state.pressureToRelease
+        const nextPressure = state.pressure + (maxTurns - state.time + 1) * state.pressureToRelease
         if (nextPressure > maxPressure) {
             maxPressure = nextPressure
             maxState = state
         }
-        nextActions(g, state).forEach(a => q.add(applyAction(g, state, a)))
+        nextActions(g, state, maxTurns, openableValves).forEach(a => q.push(applyAction(g, state, a)))
     }
-
-    console.log({ explored }, maxState)
+    // console.log({ explored }, maxState)
     return maxPressure
 }
 
 function printGraph(g: Graph) {
     for (const n in g) {
-        console.log(`${n} rate=${g[n].rate} links=${g[n].links}`)
+        console.log(`${n} rate=${g[n].rate} dists=${Object.entries(g[n].dists ?? {}).map(([to, distance]) => `${to}=${distance}`).join(', ')}`)
     }
 }
 
 export function part1(input: string) {
     const g = parseInput(input)
-    printGraph(g)
-    return run(g)
+    compressGraph(g)
+    // printGraph(g)
+    const openable = new Set(Object.keys(g).filter(n => g[n].rate > 0))
+    return run(g, 30, openable)
 }
 
-// brute force...
+function splitCombos3(valves: Set<string>): Set<string>[] {
+    const l = Array.from(valves)
+    const res: Set<string>[] = []
+    for (let a = 0; a < l.length - 3; a++) {
+        for (let b = a + 1; b < l.length - 3 + 1; b++) {
+            for (let c = b + 1; c < l.length - 3 + 2; c++) {
+                res.push(new Set([l[a], l[b], l[c]]))
+            }
+        }
+    }
+    return res
+}
+
+function splitCombos7(valves: Set<string>): Set<string>[] {
+    const l = Array.from(valves)
+    const res: Set<string>[] = []
+    for (let a = 0; a < l.length - 7; a++) {
+        for (let b = a + 1; b < l.length - 7 + 1; b++) {
+            for (let c = b + 1; c < l.length - 7 + 2; c++) {
+                for (let d = c + 1; d < l.length - 7 + 3; d++) {
+                    for (let e = d + 1; e < l.length - 7 + 4; e++) {
+                        for (let f = e + 1; f < l.length - 7 + 5; f++) {
+                            for (let g = f + 1; g < l.length - 7 + 6; g++) {
+                                res.push(new Set([l[a], l[b], l[c], l[d], l[e], l[f], l[g]]))
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return res
+}
+
 export function part2(input: string) {
+    // we explore all set combinations for 2 visitors
+    // each player can open only a subset of the valves assigned in advance
+    const g = parseInput(input)
+    compressGraph(g)
+    const openable = new Set(Object.keys(g).filter(n => g[n].rate > 0))
+    let maxPressure = 0
+    splitCombos7(openable).forEach(openable1 => {
+        const openable2 = new Set(_.difference(Array.from(openable), Array.from(openable1)))
+        // console.log(openable1, openable2)
+        maxPressure = Math.max(maxPressure, run(g, 26, openable1) + run(g, 26, openable2))
+    })
+    return maxPressure
 }
